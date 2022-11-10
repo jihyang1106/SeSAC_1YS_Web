f550db993dd385a22f487dd80ea592aae6aae96f
jihyang <jihyangi043@gmail.com>
2022-11-10 18:43:19 +0900

    feat: API 연습

--- API_practice/script.js
+++ API_practice/script.js
@@ -1,12 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new TypeIt("#title_h1")
+    .pause(1000) // 1초
+    .go();
+});
+
 $("#search").click(function () {
   $.ajax({
     method: "GET",
     url: "https://dapi.kakao.com/v3/search/book?target=title",
     data: { query: $("#bookSearch").val() },
     headers: {
-      Authorization: "KakaoAK ###############",
+      Authorization: "KakaoAK XXXXXXXXXXX",
     },
-  }).done(function (msg) {
-    console.log(msg);
+  }).done(function (res) {
+    $("#bookImg").append("<img src='" + res.documents[0].thumbnail + "'/>");
+    $(".card-title").append(res.documents[0].title);
   });
 });
