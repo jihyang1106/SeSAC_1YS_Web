964b038717f5ea9d692cc50a44523473ba5f9329
jihyang <jihyangi043@gmail.com>
2022-12-06 21:03:38 +0900

    feat : sequelize 진도 코드 수정

--- sequelize-init/controller/Cvisitor.js
+++ sequelize-init/controller/Cvisitor.js
@@ -1,136 +1,161 @@
-// const Visitor = require("../model/Visitor");
-// 객체 구조 분해 할당으로 db 안에 visitor를 가져오겠다.
-// == const { Visitor } = require("../model/index");  
+// 객체 구조 분해 할당으로 db 안에 visitor를 가져온다.
 const { Visitor } = require("../model");
+const { Op } = require("sequelize"); // where안에 조건절을 위해
 
+// visitor 페이지 렌더 (SELECT)
+// (1) promise 객체
+// exports.visitor = (req, res) => {
+//     Visitor.findAll() 
+//     .then((result)=>{
+//         console.log("visitor result : ", result);
+//         console.log("result id : ", result[0].id);
+//         res.render("visitor", {data :result});
+//     })
+// }
+
+// (2) async await 
 exports.visitor = async (req, res) => {
-    /* sequelize안에 있는 findAll함수 == SELECT문
-       sequelize는 promise 객체여서 콜백함수 필요없음
-       대신 결과값이 나오고 나서 실행하는 함수를 then으로!
-    */
-    // Visitor.findAll() 
-    // .then((result)=>{
-    //     console.log(result);
-    //     console.log("result id : ", result[0].id);
-    //     res.render("visitor", {data :result});
-    // })
-
-    // Visitor.findAll() 결과가 나올때까지 기다리고 난 뒤 결과가 result 안에 들어간다.
-    // async 함수안에 await을 사용할 수 있다. 
     let result = await Visitor.findAll(); 
-    console.log(result);
-    res.render("visitor", {data : result});
-    
-    // SELECT * FROM visitor;
-    // Visitor.get_visitor(function(result){
-    //     console.log(result);
-    //     res.render("visitor", {data: result});
-    // })
+    res.render("visitor", {data : result})
 }
 
-// INSERT INTO visitor
+// visitor 등록 (INSERT)
+// (1) promise 객체
+// exports.register = (req, res) => {
+//     let data = {
+//         name : req.body.name,
+//         comment : req.body.comment
+//     }
+//     Visitor.create(data)
+//     .then((result)=>{
+//         console.log("insert result : ", result);
+//         console.log("result id: ", result.id)
+//         res.send(String(result.id));
+//     })
+// }
+
+// (2) async await
 exports.register = async (req, res) => {
     let data = {
-        // key는 컬럼이름과 같아야 한다.
         name : req.body.name,
         comment : req.body.comment
     }
-    // Visitor.create(data) // 무조건 인자가 들어가야한다. 
-    // .then((result)=>{
-    //     console.log("insert result : ", result);
-    //     // console.log("result id: ", result.id)
-    //     res.send(String(result.id));
-    // })
     let result = await Visitor.create(data);
-    console.log("insert result : ", result);
     res.send(String(result.id));
-
-    // INSER INTO visitor(name, comment) values('${req.body.name}', '${req.body.comment}');
-    // Visitor.register_visitor( req.body, function(id){
-    //     console.log(id);
-    //     res.send(String(id));
-    // })
 }
 
-// DELETE
-exports.delete = async (req, res) => {
-    // Visitor.destroy({
-    //     where : {id : req.body.id}
-    // })
-    // .then((result)=>{
-    //     console.log("destory result : ", result);
-    //     res.send(true);
-    // })
-    let result = await Visitor.destroy({
-        where : {id : req.body.id}
-    })
-    console.log("destory result: ", result);
-    res.send(true);
-    // DELETE FROM visitor WHERE id = '${req.body.id}';
-    // Visitor.delete_visitor(req.body.id, function(){
-    //     res.send(true);
-    // })
-}
+// updateLoad (SELECT get 조회)
+// (1) promise 객체
+// exports.get_visitor_by_id = (req, res) => {
+//     Visitor.findOne({ 
+//         where : { id : req.query.id } 
+//     }) 
+//     .then((result)=>{
+//         res.send(result);
+//         console.log("result one : ", result);
+//         console.log("result.id: ", result.id); 
+//     })
+// }
 
-
-// updateLoad
+// (2) async await
 exports.get_visitor_by_id = async (req, res) => {
-    // 1. async await
     let result = await Visitor.findOne({
-        where : { id : req.query.id }
+        where : { id : { [Op.substring] : req.query.id } },
     });
-    console.log("result one : ", result);
     res.send(result);
-
-    // Visitor.findOne({ // sequelize안에 있는 findOne함수 ==> SELECT로 조건에 해당하는 행만
-    //     where : { id : req.query.id } // findAll과 findOne의 차이점 limit이 걸려있는지 아닌지 
-    // }) 
-    // .then((result)=>{
-    //     res.send(result);
-    //     console.log("result one : ", result);
-    // })
-
-    /* findAll로도 where 절 가능
-    // Visitor.findOne({ // sequelize안에 있는 findOne함수 ==> SELECT로 조건에 해당하는 행만
-    //     where : { id : req.query.id },
-    //     limit : 1
-        }) */
-
-
-
-    // SELECT * FROM visitor WHERE id = "req.query.id";
-    // // req.query.id 에 해당하는 데이터를 조회
-    // // 서버 응답 > 조회한 데이터
-    // Visitor.get_visitor_by_id_model(req.query.id, function(rows){
-    //     res.send(rows);
-    // });
 }
 
-// UPDATE
+
+// visitor 수정 (UPDATE)
+// (1) promise 객체
+// exports.update_visitor =  (req, res) => {
+//     // 첫 번째 인자 데이터
+//     let data = {
+//         name : req.body.name,
+//         comment : req.body.comment
+//     }
+//     // 두 번째 인자 where 절
+//     Visitor.update(data,{
+//         where : {id : req.body.id}
+//     })
+//     .then((result)=>{
+//         console.log("update result: ", result);
+//         console.log("update result[0]: ", result[0]);
+//         res.send(true);
+//     })
+// }
+
+// (2) async awit
 exports.update_visitor = async (req, res) => {
-    // 첫 번째 인자
     let data = {
         name : req.body.name,
         comment : req.body.comment
     }
-    // 두 번째 인자 where 절
-    // Visitor.update(data,{
-    //     where : {id : req.body.id}
-    // })
-    // .then((result)=>{
-    //     console.log("update result : ", result);
-    //     res.send(true);
-    // })
-    let result = await Visitor.update(data, {
+    await Visitor.update(data, {
         where : {id : req.body.id}
-    }).then((result)=>{
-        console.log("update result : ", result);
+    }).then(()=>{
         res.send(true);
     })
-    // UPDATE visitor SET name='${req.body.name}', comment='${req.body.comment}' WHERE id='${req.body.id}}'
-    // 서버의 응답 
-    // Visitor.update_visitor(req.body, function(){
-    //     res.send(true);
-    // });
 }
 
+// visitor 삭제 (DELETE)
+// (1) promise 객체
+// exports.delete =  (req, res) => {
+//     Visitor.destroy({
+//         where : {id : req.body.id}
+//     })
+//     .then((result)=>{
+//         console.log("destory result : ", result);
+//         console.log("destory result typeof : ", typeof(result));
+//         res.send(true);
+//     })
+// }
+
+// (2) async await
+exports.delete = async (req, res) => {
+    await Visitor.destroy({
+        where : {id : req.body.id}
+    })
+    res.send(true);
+}
+
+// sql 쿼리문과 연동 된 것 (기존 코드)
+// const Visitor = require("../model/Visitor");
+
+// visitor 페이지 렌더
+// exports.visitor = (req, res) => {    
+//     Visitor.get_visitor(function(result){
+//         res.render("visitor", {data: result});
+//     })
+// }
+
+// visitor 등록
+// exports.register = (req, res) => {
+//     Visitor.register_visitor( req.body, function(id){
+//         res.send(String(id));
+//     })
+// }
+
+// updateLoad (SELECT)
+// exports.get_visitor_by_id = async (req, res) => {
+//     // req.query.id 에 해당하는 데이터를 조회
+//     // 서버 응답 > 조회한 데이터
+//     Visitor.get_visitor_by_id_model(req.query.id, function(rows){
+//         res.send(rows);
+//     });
+// }
+
+// visitor 수정 (UPDATE)
+// exports.update_visitor = (req, res) => {
+//     // 서버의 응답 
+//     Visitor.update_visitor(req.body, function(){
+//         res.send(true);
+//     });
+// }
+
+// visitor 삭제 (DELETE)
+// exports.delete = async (req, res) => {
+//     Visitor.delete_visitor(req.body.id, function(){
+//         res.send(true);
+//     })
+// }
--- sequelize-init/model/index.js
+++ sequelize-init/model/index.js
@@ -13,19 +13,10 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
 // 모델이름과 동일하게하기
-// 괄호가 있다 => 함수를 실행시키겠다.
-// Sequlize.define()으로 모델을 정의할 수 있다. 
-// 함수를 실행시켜야지 Visitor 함수의 return 값을 받아올 수 있다. 
-// sequelize 모듈 호출해온것을 두 번째 매개변수로
-// sequelize안에 DataTypes을 사용하기 위해 두 번째 인자로 보냈다.
+// 괄호가 있다 => 함수를 실행한다. => 함수의 return 값을 받는다.
+// sequelize의 db 정보(config)와 모델을 정의 한 것을 connect하기 위해 첫 번째 인자
+// Sequelize의 DataTypes을 사용하기 위해 두 번째 인자
 db.Visitor = require("./Visitor")(sequelize, Sequelize);
-
-// db = { 
-//     "sequlize" : sequelize,
-//     "Sequelize" : Sequelize,
-//     "visitor" : "Visitor.js에서 return 받은 값"
-// }
 
 module.exports = db;
