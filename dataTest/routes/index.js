462f42caedb9f19516e688fb3b7b50acc06a027f
jihyang <jihyangi043@gmail.com>
2022-12-15 18:02:26 +0900

    데이터 크롤링

--- dataTest/routes/index.js
+++ dataTest/routes/index.js
@@ -8,7 +8,9 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const { json } = require("body-parser");
 
+// DB에 넣을 데이터 객체 (recipe_title, recipe_url, recipe_img, recipe_ingd, recipe_time )
 router.get("/test", async (req,res) => {
+    
     await axios({
         method : 'get',
         url : "https://www.10000recipe.com/ranking/home_new.html?rtype=r&dtype=d",
@@ -16,28 +18,49 @@ router.get("/test", async (req,res) => {
         const $ = cheerio.load(res.data);
         const list = $("ul.common_sp_list_ul > li.common_sp_list_li");
 
-        // title과 url 넣기
-        let jsonList = {};
-        list.each((i,list)=> {
+        let dbList = [];
+    
+        // 만약 데이터 한 개로 테스트 하려면 each 없이 바로 axios로 요청 가능
+        list.each(async (i,list) => {
             const title = $(list).find("div.common_sp_caption_tit").text();
             const url = "http://10000recipe.com"+$(list).find("a.common_sp_link").attr('href');
-
-            jsonList[i] = {title,url};
-            // console.log(i+1, {
-            //     title,
-            //     url
-            // })
+  
+            await axios({
+                method : 'get',
+                url : url,
+            }).then((res)=>{ 
+                const $ = cheerio.load(res.data);  // $("특정태그")
+                const time = $("body").find("span.view2_summary_info2").text();
+                const ingre = $("div.ready_ingre3 ul:first-child > li > a:first-child").text().replace(/ /gi, ""); // 공백제거
+                const ingd = ingre.slice(0, -1); // 마지막 공백 제거
+                const img = $("img#main_thumbs").attr('src');
+                
+                dbList.push({
+                    title : title,
+                    url : url, 
+                    time : time,
+                    ingd : ingd,
+                    img : img,
+                })
+                return;
+            });
+            console.log(dbList);
         });
-        for(var i=0; i<Object.values(jsonList).length; i++){
-            console.log(jsonList[i].url);
-        };
-        
-    
-        
+
         
     }).catch(err => {
         console.log(err);
+        // for(var i=0; i<Object.values(jsonList).length; i++){
+        //     console.log(jsonList[i].url);
+        // }
     });
+})
+
+router.get("/test2", async (req,res) => {
+    for(var i=0; i<Object.values(jsonList).length; i++){
+        console.log(jsonList[i].title);
+    }
+    
 })
 
 
