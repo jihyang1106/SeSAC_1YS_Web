981da053a6eff66dc9b6dacf68e9a53de74404ee
jihyang <jihyangi043@gmail.com>
2023-01-03 18:44:34 +0900

    feat: react 1

---
+++ react-my-app/src/components/ButtonNaver.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';  
+
+class BtnToNaver extends Component {
+    render() {
+        return (
+            <a href="https://www.naver.com/">네이버로 이동</a>
+        )
+    }
+}
+
+export default BtnToNaver;
---
+++ react-my-app/src/components/ImgComponent.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+
+// 실습 클래스형 컴포넌트, 함수형 컴포넌트
+function ImgComponent() {
+    return (
+        <img src={logo} alt="로고"/>
+    )
+}
+
+
+export default ImgComponent;
