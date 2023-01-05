b765576ef85b4c41e47caca7b4b146e0551c5492
jihyang Lee <94888511+jihyang1106@users.noreply.github.com>
2023-01-05 18:50:19 +0900

    Delete Nodejs directory

--- Nodejs/01_module.js
+++
@@ -1,9 +0,0 @@
-// 모듈로 선언하는 파일
-const a = 1;
-const b = 3;
-
-module.exports = {
-    a,
-    b
-}
-
--- Nodejs/01_module3.js
+++
@@ -1,5 +0,0 @@
-// module2.js의 모듈을 불러와서 실행
-const result = require('./01_module2.js');
-
-console.log(result);
-console.log(result());
