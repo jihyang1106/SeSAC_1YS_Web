c2fa307f910490a40b8d7ec328315a06868049a8
jihyang Lee <94888511+jihyang1106@users.noreply.github.com>
2023-01-05 18:50:06 +0900

    Delete MVCPractice directory

--- MVCPractice/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const controller = require("../controller/Cmain");
-const router = express.Router();
-
-router.get("/", controller.main);
-// router.post("/login", controller.axios);
-router.post("/login2", controller.loginPost);
-
-module.exports = router;
